bcfec9bb88d6dc57b146b7b13acd197d63930f2b
mkspramuditha <mkspramuditha@gmail.com>
2016-12-08 16:50:21 +0530

    button and a text field added

--- app/src/main/java/com/example/shan/mqttclient/MainActivity.java
+++ app/src/main/java/com/example/shan/mqttclient/MainActivity.java
@@ -2,47 +2,89 @@ package com.example.shan.mqttclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
-                        clientId);
+        final EditText textfield = (EditText)findViewById(R.id.editText);
+        Button button = (Button)findViewById(R.id.button);
+
+        final MqttAndroidClient mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), "tcp://128.199.217.137:1883", "androidSampleClient");
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("Connection was lost!");
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Message Arrived!: " + topic + ": " + new String(message.getPayload()));
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("Delivery Complete!");
+            }
+        });
+
+        button.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        try {
+                            String text = textfield.getText().toString();
+                            mqttAndroidClient.publish("test", new MqttMessage(text.getBytes()));
+                        } catch (MqttException ex) {
+
+                        }
+                    }
+
+                }
+        );
 
         try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener() {
-                public static final String TAG = "connection" ;
-
+            mqttAndroidClient.connect(null, new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connectede
-                    Log.d(TAG, "onSuccess");
+                    System.out.println("Connection Success!");
+                    try {
+                        System.out.println("Subscribing to /test");
+                        mqttAndroidClient.subscribe("test", 0);
+                        System.out.println("Subscribed to /test");
+                        System.out.println("Publishing message..");
+                        mqttAndroidClient.publish("test", new MqttMessage("Hello world!".getBytes()));
+                    } catch (MqttException ex) {
+
+                    }
+
                 }
 
                 @Override
                 public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Log.d(TAG, "onFailure");
-
+                    System.out.println("Connection Failure!");
                 }
             });
-        } catch (MqttException e) {
-            e.printStackTrace();
+        } catch (MqttException ex) {
+
         }
     }
 }
