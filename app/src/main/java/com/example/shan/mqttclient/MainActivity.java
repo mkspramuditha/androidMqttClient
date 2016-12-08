de5cda82e82bd590e35c2e217a9b4850090c27aa
mkspramuditha <mkspramuditha@gmail.com>
2016-12-08 15:18:40 +0530

    mqtt client started. connection part added

--- app/src/main/java/com/example/shan/mqttclient/MainActivity.java
+++ app/src/main/java/com/example/shan/mqttclient/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.shan.mqttclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String clientId = MqttClient.generateClientId();
+        MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
 
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                public static final String TAG = "connection" ;
+
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connectede
+                    Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
